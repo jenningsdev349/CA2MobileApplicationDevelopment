f281287bda594736f693496cd25df95c88e9e4ab
Adam Jennings <adamjennings349@gmail.com>
2025-11-15 22:42:32 +0000

    - Added sign in and log in methods - Built out UI for sign in and log in screens - Added strings

--- app/src/main/java/com/jenningsdev/ca2mobileapplicationdevelopment/Activities/SignInActivity.java
+++ app/src/main/java/com/jenningsdev/ca2mobileapplicationdevelopment/Activities/SignInActivity.java
@@ -1,16 +1,29 @@
 package com.jenningsdev.ca2mobileapplicationdevelopment.Activities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.jenningsdev.ca2mobileapplicationdevelopment.R;
 
 public class SignInActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,28 @@ public class SignInActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
+    public void signIn(View view) {
+        EditText emailEditText = findViewById(R.id.email_editText);
+        EditText passwordEditText = findViewById(R.id.password_editText);
+
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("BANANA", "Logged in.");
+                            mUser = mAuth.getCurrentUser();
+                        } else {
+                            Toast.makeText(SignInActivity.this, "Authentication failed. Please try again.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
--- app/src/main/java/com/jenningsdev/ca2mobileapplicationdevelopment/Activities/SignUpActivity.java
+++ app/src/main/java/com/jenningsdev/ca2mobileapplicationdevelopment/Activities/SignUpActivity.java
@@ -1,16 +1,31 @@
 package com.jenningsdev.ca2mobileapplicationdevelopment.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.credentials.CreateCredentialRequest;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.jenningsdev.ca2mobileapplicationdevelopment.R;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +37,32 @@ public class SignUpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
+    public void signUp(View view) {
+        EditText emailEditText = findViewById(R.id.email_editText);
+        EditText passwordEditText = findViewById(R.id.password_editText);
+
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("myActivity", "createUserWithEmail:success");
+                            mUser = mAuth.getCurrentUser();
+                            Intent intent = new Intent(getApplicationContext(), CreateCredentialRequest.DisplayInfo.class);
+                            startActivity(intent);
+                        } else {
+                            Log.w("myActivity", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
